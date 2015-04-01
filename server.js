fccf03b74eb80a3559ddd33d0e5ba315d05ec8f7
Marc!! <marc.bastien@gmail.com>
2015-04-01 23:48:55 +0000

    WHAT: Added templates

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
+app.use(express.static(__dirname + "/client"));
+
 app.get("/", function(req, res){
     fs.readFile(__dirname + "/index.html", function(err, html){
         res.send(html.toString());
